be069741c022f6d37e714701197e64d99d664ca9
Phuoc <shishima21@gmail.com>
2019-03-31 17:57:09 +0700

    Session 10 Dynamic class names

--- Todo-List/src/App.js
+++ Todo-List/src/App.js
@@ -1,29 +1,20 @@
-import React, { Component } from "react";
-import "./App.css";
-import "./components/TodoItem";
-import TodoItem from "./components/TodoItem";
+import React, { Component } from 'react';
+import './App.css';
+import './components/TodoItem';
+import TodoItem from './components/TodoItem';
 
 class App extends Component {
-    constructor() {
-        super();
-        this.Items = [
-            "Go shopping",
-            "Go swimming",
-            "Go home"
-        ];
-    }
-    render() {
-        return (
-            <div className="App">
-            {
-                this.Items.map(
-                    (item, index) => <TodoItem key={index} title={item} />
-                    )
-            }
-                
-            </div>
-        );
-    }
+	constructor() {
+		super();
+		this.Items = [
+			{ title: 'Go shopping', isComplete: true },
+			{ title: 'Go swimming', isComplete: false },
+			{ title: 'Go home', isComplete: false }
+		];
+	}
+	render() {
+		return <div className="App">{this.Items.map((item, index) => <TodoItem key={index} item={item} />)}</div>;
+	}
 }
 
 export default App;
--- Todo-List/src/components/TodoItem.js
+++ Todo-List/src/components/TodoItem.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-
+import './TodoItem.css';
 class TodoItem extends Component {
-    render() {
-        return  (
-            <div className="TodoItem">
-                <p className="text-primary">{this.props.title}</p>
-            </div>
-        )
-    }
+	render() {
+		const { item } = this.props;
+		let className = 'TodoItem';
+		if (item.isComplete) {
+			className += ' TodoItem_Complete';
+		}
+
+		return (
+			<div className={className}>
+				<p>{item.title}</p>
+			</div>
+		);
+	}
 }
 
 export default TodoItem;
