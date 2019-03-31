64f2af1b750a24e8d98e59220e2bd50d7963f8d4
Phuoc <shishima21@gmail.com>
2019-03-31 18:30:48 +0700

    Session 11Conditional Rendering

--- Todo-List/src/App.js
+++ Todo-List/src/App.js
@@ -1,20 +1,51 @@
-import React, { Component } from 'react';
-import './App.css';
-import './components/TodoItem';
-import TodoItem from './components/TodoItem';
+import React, { Component } from 'react'
+import './App.css'
+import './components/TodoItem'
+import TodoItem from './components/TodoItem'
 
 class App extends Component {
-	constructor() {
-		super();
-		this.Items = [
-			{ title: 'Go shopping', isComplete: true },
-			{ title: 'Go swimming', isComplete: false },
-			{ title: 'Go home', isComplete: false }
-		];
-	}
-	render() {
-		return <div className="App">{this.Items.map((item, index) => <TodoItem key={index} item={item} />)}</div>;
-	}
+    constructor() {
+        super()
+        this.TodoItems = [
+            { title: 'Go shopping', isComplete: true },
+            { title: 'Go swimming', isComplete: false },
+            { title: 'Go home', isComplete: false }
+        ]
+    }
+
+    // Option 2:
+    render() {
+        return (
+            <div className='App'>
+                {this.TodoItems.length > 0 && this.TodoItems.map((item, index) => (
+                    <TodoItem key={index} item={item} />
+                ))}
+                {
+                    this.TodoItems.length === 0 && "Nothing Here!"
+                }
+            </div>
+        );
+    }
+
+    // Option 2:
+    /* render() {
+        if (this.Items.length > 0) {
+            return (
+                <div className='App'>
+                    {this.Items.map((item, index) => (
+                        <TodoItem key={index} item={item} />
+                    ))}
+                </div>
+            );
+                    {this.Items.map((item, index) => (
+                        <TodoItem key={index} item={item} />
+                    ))}
+                </div>
+            );
+        } else {
+            return (<div className='App'>Nothings Here.</div>)
+        }
+    }*/
 }
 
-export default App;
+export default App
--- Todo-List/src/components/TodoItem.js
+++ Todo-List/src/components/TodoItem.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import './TodoItem.css';
+import React, { Component } from "react";
+import "./TodoItem.css";
 class TodoItem extends Component {
-	render() {
-		const { item } = this.props;
-		let className = 'TodoItem';
-		if (item.isComplete) {
-			className += ' TodoItem_Complete';
-		}
+    render() {
+        const { item } = this.props;
+        let className = "TodoItem";
+        if (item.isComplete) {
+            className += " TodoItem_Complete";
+        }
 
-		return (
-			<div className={className}>
-				<p>{item.title}</p>
-			</div>
-		);
-	}
+        return (
+            <div className={className}>
+                <p>
+                    {item.title}
+                </p>
+            </div>
+        );
+    }
 }
 
 export default TodoItem;
