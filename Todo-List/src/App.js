15f5ae224c4216f1477175074bc282347d59bbaf
Phuocnd <shishima21@gmail.com>
2019-04-14 09:52:32 +0700

    complete

--- Todo-List/src/App.js
+++ Todo-List/src/App.js
@@ -2,22 +2,25 @@ import React, { Component } from 'react'
 import './App.css'
 import './components/TodoItem'
 import TodoItem from './components/TodoItem'
+import tick from './img/tick.svg'
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
+            newItem: '',
             TodoItems: [
                 { title: 'Go shopping', isComplete: true },
                 { title: 'Go swimming', isComplete: false },
                 { title: 'Go home', isComplete: false }
             ]
         }
+        this.onKeyUp = this.onKeyUp.bind(this);
+        this.onChange = this.onChange.bind(this);
     }
 
     onItemClick(item) {
         return () => {
-            console.log(item);
             const isComplete = item.isComplete;
             const { TodoItems } = this.state;
             const index = TodoItems.indexOf(item);
@@ -34,17 +37,49 @@ class App extends Component {
         }
     }
 
+    onKeyUp(e) {
+        if (e.keyCode === 13) {
+            let txt = e.target.value.trim();
+            if (!txt) {
+                return;
+            }
+
+            this.setState({
+                newItem: '',
+                TodoItems: [
+                    { title: txt, isComplete: false },
+                    ...this.state.TodoItems
+                ]
+            })
+        }
+
+    }
+    onChange(e) {
+        this.setState({
+            newItem: e.target.value
+        })
+    }
+
     render() {
         if (this.state.TodoItems.length) {
             return (
-                < div className='App' >
+                <div className='App' >
+                    <div className="Header">
+                        <img src={tick} alt={tick} className="imgSize" />
+                        <input
+                            type="text"
+                            placeholder="Add to new item"
+                            value={this.state.newItem}
+                            onChange={this.onChange}
+                            onKeyUp={this.onKeyUp} />
+                    </div>
                     {this.state.TodoItems.map((item, index) =>
                         <TodoItem
                             key={index}
                             item={item}
                             onClick={this.onItemClick(item)} />
                     )}
-                </div >
+                </div>
             )
         } else {
             return (
--- Todo-List/src/components/TodoItem.js
+++ Todo-List/src/components/TodoItem.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 import "./TodoItem.css";
 import classNames from "classnames";
+import checked from '../img/checked.svg';
+import uncheck from '../img/uncheck.svg';
 class TodoItem extends Component {
     render() {
         const { item, onClick } = this.props;
+        let url = uncheck;
+        if (item.isComplete) {
+            url = checked;
+        }
+
         return (
-            <div onClick={onClick} className={classNames('TodoItem', {
+            <div className={classNames('TodoItem', {
                 'TodoItem_Complete': item.isComplete
             })}>
+                <img onClick={onClick} src={url} alt={"logo"} className="imgSize" />
                 <p>
                     {item.title}
                 </p>
